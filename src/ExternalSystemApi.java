64c8154a726ed7003666735d88eda64b22741b40
Victor Augusto <victoraugusto2812@gmail.com>
2024-07-18 21:45:02 -0300

    "Adapter"

---
+++ src/ExternalSystemApi.java
@@ -0,0 +1,13 @@
+public class ExternalSystemApi {
+    public String findBooksTitle(String title){
+        return ("Searching for books by the title "+ title+" externally");
+    }
+
+    public String findBooksAuthor(String author){
+        return ("Searching for books by the title "+ author+" externally");
+    }
+
+    public String findBooksCategory(String category){
+        return ("Searching for books by the title "+ category+" externally");
+    }
+}
